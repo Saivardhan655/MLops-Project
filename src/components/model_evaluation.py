169e79cdf84297143e6e4ac8a3af52242a9e7bfa
Saivardhan655 <saivardhan9154@gmail.com>
2024-07-23 11:39:22 +0530

    code for mlflow edited

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from src.logger.custom_logger import logging
 from src.exception.exception import customexception
+from src.utils.utils import load_object
 import os
 import sys
 import mlflow
@@ -16,10 +17,32 @@ class ModelEvaluationConfig:
 class ModelEvaluation:
     def __init__(self):
         pass
-    def initiate_model_evaluation(self):
+    def eval_metrics(self,actual,pred):
+        rmse=np.sqrt(mean_squared_error(actual,pred))
+        mae=mean_absolute_error(actual,pred)
+        r2=r2_score(actual,pred)
+        return rmse,mae,r2
+    def initiate_model_evaluation(self,train_array,test_array):
         try:
-            pass
+            X_test,y_test=(test_array[:,:-1],test_array[:,-1])
+            model_path=os.path.join("artifacts","model.pkl")
+            model=load_object(model_path)
+            mlflow.set_registry_uri("")
+            logging.info("model had registered")
+            tracking_url_type_store=urlparse(mlflow.get_tracking_uri()).scheme
+            print(tracking_url_type_store)
+
+            with mlflow.start_run():
+                prediction=model.predict(X_test)
+                (rmse,mae,r2)=self.eval_metrics(y_test,prediction)
+                mlflow.log_metric("rmse",rmse)
+                mlflow.log_metric("mae",mae)
+                mlflow.log_metric("r2",r2)
+                if tracking_url_type_store!="file":
+                    mlflow.sklearn.log_model(model,"model",registered_model_name="ml_model")
+                else:
+                    mlflow.sklearn.log_model(model,"model")
         except Exception as e:
-            logging.info()
+            logging.info("model not found")
             raise customexception(e,sys)
         
